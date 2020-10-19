14d0f66dc8e2fd2650203e9b5cf6da9bc3aaadde
Buq <73091194+buqsucks@users.noreply.github.com>
2020-10-19 00:16:07 -0400

    Add files via upload
    Added files for package

---
+++ CCS/Primary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CCS
+{
+    public class Primary
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int FreeConsole();
+        private void Call()
+        {
+            AllocConsole();
+        }
+        private void End()
+        {
+            FreeConsole();
+        }
+        private void Resize(int width, int height)
+        {
+            Console.WindowHeight = height;
+            Console.BufferHeight = height;
+            Console.WindowWidth = width;
+            Console.BufferWidth = width;
+        }
+    }
+}
